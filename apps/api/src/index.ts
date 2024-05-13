11565f054927e2595775b35af9007c8b4a4de5bf
Fernando D'luccas <dluccash@hotmail.com>
2024-05-13 17:55:37 -0300

    feat: permissions by roles

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,6 @@
-import { ability } from "@saas/auth";
+import { defineAbilitiesFor } from "@saas/auth";
 
+const ability = defineAbilitiesFor({ role: 'ADMIN' })
 
 const userCanInviteSomeone = ability.can('invite', 'User')
 const userCanDeleteUser = ability.can('delete', 'User')
---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,17 @@
+import { AbilityBuilder } from "@casl/ability"
+import { AppAbility } from "."
+import { User } from "./models/user"
+
+type PermissionsByRole = (User: User, builder: AbilityBuilder<AppAbility>) => void
+type Role = 'ADMIN' | 'MEMBER'
+export const permissions: Record<Role,PermissionsByRole> = {
+    ADMIN(_, {can}){
+        can('manage', 'all')
+
+
+    },
+    MEMBER(_, {can }){
+        can('invite', 'User')
+    }
+
+}
