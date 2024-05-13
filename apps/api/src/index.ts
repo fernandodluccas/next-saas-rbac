093caaee00c8a94737ff53e5df0ed3878dc46dcc
Fernando D'luccas <dluccash@hotmail.com>
2024-05-13 17:41:41 -0300

    feat: creating initial permissions

---
+++ apps/api/src/index.ts
@@ -0,0 +1,8 @@
+import { ability } from "@saas/auth";
+
+
+const userCanInviteSomeone = ability.can('invite', 'User')
+const userCanDeleteUser = ability.can('delete', 'User')
+
+console.log(userCanInviteSomeone) // true
+console.log(userCanDeleteUser) // false
