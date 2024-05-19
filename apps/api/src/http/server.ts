7a7c79a3fe284f0e4c6c2c6bc423c40ea5f6ae26
Fernando D'luccas <dluccash@hotmail.com>
2024-05-19 18:59:45 -0300

    feat: set up Swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,12 +1,36 @@
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createAccount } from "./routes/auth/create-account";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify();
 
+
+
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "Next.js SaaS",
+            description: "Fullstack SaaS app with multi-tentant & RBAC",
+            version: "0.1.0",
+        },
+        servers: [
+        ],
+
+    },
+    transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+
+
+})
 
 app.register(fastifyCors)
 
