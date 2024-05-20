9de932fb71c5fefa7e174a3d80d54bc376c50a1c
Fernando D'luccas <dluccash@hotmail.com>
2024-05-20 13:12:08 -0300

    feat: route: Logged-in user profile

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,53 @@
+import { prisma } from "@/lib/prisma";
+import { FastifyInstance } from "fastify";
+import z from "zod";
+
+export async function getProfile(app: FastifyInstance) {
+    app.get(
+        '/profile',
+        {
+            schema: {
+                tags: ["auth"],
+                summary: "Get authenticated user profile",
+                response: {
+                    200: z.object({
+                        id: z.string(),
+                        email: z.string(),
+                        name: z.string().optional(),
+                        avatarUrl: z.string().optional(),
+                    }),
+                }
+                ,
+                401: {
+
+                },
+            },
+        },
+
+        async (request, reply) => {
+            const { sub } = await request.jwtVerify<{ sub: string }>();
+
+            const user = await prisma.user.findUnique({
+                select: {
+                    id: true,
+                    email: true,
+                    name: true,
+                    avatarUrl: true,
+                },
+                where: {
+                    id: sub,
+                },
+            });
+
+            if (!user) {
+                throw new Error("User not found");
+            }
+
+            return reply.send({ user });
+
+
+        }
+
+    );
+
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -10,6 +10,7 @@ import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
 import fastifyJwt from "@fastify/jwt";
+import { getProfile } from "./routes/auth/get-profile";
 
 const app = fastify();
 
@@ -40,6 +41,7 @@ app.register(fastifyCors);
 
 app.register(createAccount);
 app.register(authenticateWithPassword);
+app.register(getProfile);
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP Server running");
