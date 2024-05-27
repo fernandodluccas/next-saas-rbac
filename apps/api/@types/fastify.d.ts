76620c0301ca79ce2c7433dbe54e3df5ae72f062
Fernando D'luccas <dluccash@hotmail.com>
2024-05-27 20:00:51 -0300

    feat: retrieve membership

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
+import { Member, Organization } from "@prisma/client";
 import "fastify";
 
 declare module 'fastify' {
     export interface FastifyRequest {
-        getCurrentUserId: () => Promise<string>;
+        getCurrentUserId: () => Promise<string>
+        getUserMembership: (slug: string) => Promise<{
+            organization: Organization,
+            membership: Member
+        }>;
     }
 }
